6ff444a1638472af2b2a7918099c1bd59f8525f6
edinpeter <edin.3@osu.edu>
2016-02-29 18:54:58 -0500

    Cleaned up imports

--- RiptideVision.h
+++ RiptideVision.h
@@ -1,25 +1,3 @@
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include <ctime>
-#include "geometry_msgs/Vector3.h"
-#include "std_msgs/Float32.h"
-#include <math.h>
-#include "imu_3dm_gx4/FilterOutput.h"
-#include "sensor_msgs/Joy.h"
-#include <std_msgs/Float32MultiArray.h>
-#include <opencv2/opencv.hpp>
 
 class RiptideVision{
 public:
